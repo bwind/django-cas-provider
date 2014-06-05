42e066d850a3a8e599a21fbc4b773a7053c03dea
Bas Wind <mailtobwind@gmail.com>
2014-06-05 15:30:53 +0200

    Prefer CAS_*_TICKET_MODEL, use ServiceTicket and LoginTicket as fall back

--- cas_provider/models.py
+++ cas_provider/models.py
@@ -1,18 +1,40 @@
 from django.db import models
+from django.conf import settings
 from django.contrib.auth.models import User
+from django.utils.importlib import import_module
 
-class ServiceTicket(models.Model):
-    user = models.ForeignKey(User)
-    service = models.URLField(verify_exists=False)
-    ticket = models.CharField(max_length=256)
-    created = models.DateTimeField(auto_now=True)
-    
-    def __unicode__(self):
-        return "%s (%s) - %s" % (self.user.username, self.service, self.created)
-        
-class LoginTicket(models.Model):
-    ticket = models.CharField(max_length=32)
-    created = models.DateTimeField(auto_now=True)
-    
-    def __unicode__(self):
-        return "%s - %s" % (self.ticket, self.created)
+
+ServiceTicket = None
+LoginTicket = None
+
+
+alt_models = ('CAS_SERVICE_TICKET_MODEL', 'CAS_LOGIN_TICKET_MODEL')
+if all([hasattr(settings, name) for name in alt_models]):
+    def get_model(name):
+        name = getattr(settings, name)
+        dot = name.rindex('.')
+        module = import_module(name[:dot])
+        return getattr(module, name[dot + 1:])
+    ServiceTicket = get_model(alt_models[0])
+    LoginTicket = get_model(alt_models[1])
+
+
+if not ServiceTicket:
+    class ServiceTicket(models.Model):
+        user = models.ForeignKey(User)
+        service = models.URLField()
+        ticket = models.CharField(max_length=256)
+        created = models.DateTimeField(auto_now=True)
+
+        def __unicode__(self):
+            return "%s (%s) - %s" % (self.user.username, self.service,
+                self.created)
+
+
+if not LoginTicket:
+    class LoginTicket(models.Model):
+        ticket = models.CharField(max_length=32)
+        created = models.DateTimeField(auto_now=True)
+
+        def __unicode__(self):
+            return "%s - %s" % (self.ticket, self.created)
