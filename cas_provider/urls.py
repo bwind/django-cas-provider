1934393a7b0447da1cc97f72fe3520eac92eb16e
Bas Wind <mailtobwind@gmail.com>
2014-06-11 14:43:14 +0200

    Add url names

--- cas_provider/urls.py
+++ cas_provider/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import *
 from views import *
 
 urlpatterns = patterns('',
-    url(r'^login/', login),
-    url(r'^validate/', validate),
-    url(r'^logout/', logout),
+    url(r'^login/', login, name='cas_login'),
+    url(r'^validate/', validate, name='cas_validate'),
+    url(r'^logout/', logout, name='cas_logout'),
 )
